4910a3ca0de11dbd7ece231636e22b645fc19048
Hermann Schnyder (tzhsche7) <Hermann.Schnyder@swisscom.com>
2018-12-03 09:12:33 +0100

    added cf push demo app

---
+++ cf01/readme.md
@@ -0,0 +1,8 @@
+
+
+
+ * Without Manifest (manifest.yml)  cf push demo -p build/libs/cf01-0.0.1.jar
+
+
+
+
---
+++ cf01/src/main/java/com/swisscom/oix/cf01/DemoController.java
@@ -0,0 +1,25 @@
+package com.swisscom.oix.cf01;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class DemoController {
+
+
+    @ResponseBody
+    @GetMapping("/demo")
+    public String demo(HttpServletRequest request) {
+
+        StringBuffer requestURL = request.getRequestURL();
+        String remoteAddr = request.getRemoteAddr();
+
+        return "Hello " + remoteAddr + " from  " + requestURL;
+    }
+
+
+}
